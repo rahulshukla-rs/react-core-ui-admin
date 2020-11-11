17efc9be33fe6e9b8e4acad4ccc0d5af0fccd4d7
Rahul Shukla <16shuklarahul@gmail.com>
2020-11-11 19:57:31 +0530

    firebase & env changes added

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,6 @@
-const BASE_URL = process.env.BASE_URL || "http://localhost:5000/";
+const BASE_URL = process.env.REACT_APP_PROD_API_URL || "http://localhost:5000/";
 const JWT_KEY = process.env.JWT_KEY || "testApp";
+
+console.log(process.env.REACT_APP_PROD_API_URL)
 
 export {BASE_URL, JWT_KEY}
